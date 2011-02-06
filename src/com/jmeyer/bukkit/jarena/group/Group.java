0a67e27f2aaeaa5924bbc71b1c34dd10a1e2ce68
Justian Meyer <meyer.justian@gmail.com>
2011-02-06 14:01:42 -0500

    Removed misplaced randomLocation() method

--- src/com/jmeyer/bukkit/jarena/group/Group.java
+++ src/com/jmeyer/bukkit/jarena/group/Group.java
@@ -25,37 +25,4 @@ public interface Group {
 	 */
 	public void tpAll(Location loc);
 	
-	
-	
-	// TODO: Move to own class
-	/**
-	 * Returns a random location within the boundaries given
-	 * @param world - world where location exists
-	 * @param xMin - min x of volume
-	 * @param xMax - max x of volume
-	 * @param yMin - min y of volume
-	 * @param yMax - max y of volume
-	 * @param zMin - min z of volume
-	 * @param zMax - max z of volume
-	 * @return new location within given parameters
-	 */
-	/*
-	protected Location randomLocation(World world, double xMin, double xMax, double yMin, 
-			double yMax, double zMin, double zMax) {
-		double x, y, z;
-		
-		if ((xMin > xMax) || (yMin > yMax) || (zMin > zMax)) {
-			LOG.log(Level.SEVERE, "[JArena] Could not generate random location, " + 
-					"coordinates invalid.");
-			return null;
-		} else {
-			x = (Math.random()*xMin) + (xMax-xMin);
-			y = (Math.random()*yMin) + (yMax-yMin);
-			z = (Math.random()*zMin) + (zMax-zMin);
-		}
-		
-		return new Location(world, x, y, z);
-	}
-	*/
-	
 }
