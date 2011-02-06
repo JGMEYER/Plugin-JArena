a66dd5213c04d70483d29341c624aa7e6d05b053
Justian Meyer <meyer.justian@gmail.com>
2011-02-06 12:09:26 -0500

    Converted Group to interface.

--- src/com/jmeyer/bukkit/jarena/group/Group.java
+++ src/com/jmeyer/bukkit/jarena/group/Group.java
@@ -1,94 +1,38 @@
 package com.jmeyer.bukkit.jarena.group;
 
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import org.bukkit.Location;
-import org.bukkit.World;
-import org.bukkit.craftbukkit.entity.CraftLivingEntity;
 
 /**
- * Outlines basic commands for entity groups.
+ * Outlines basic commands for battle groups.
  * @author JMEYER
  **/
-public class Group {
-
-	protected static final Logger LOG = Logger.getLogger("Minecraft");
-	// TODO: should this really be just a class? - (interface, abstract?)
-	ArrayList<CraftLivingEntity> group;
-	
-	/**
-	 * Instantiate new group with empty list.
-	 */
-	public Group() {
-		this.group = new ArrayList<CraftLivingEntity>();
-	}
-	
-	/**
-	 * Instantiate new group with given list.
-	 * @param list of LivingEntitys in group
-	 */	
-	public Group(ArrayList<CraftLivingEntity> group) {
-		this.group = group;
-	}
-	
-	
+public interface Group {
 	
 	/** 
-	 * Return the total number of living entities in the group. 
+	 * Return the total number of living members in the group. 
 	 * @return number alive
 	 */
-	public int numAlive() { 
-		int total = 0;
-		
-		for (CraftLivingEntity cle : group)
-			if (cle.getHealth() > 0)
-				total++;
-		
-		return total; 
-	}
+	public int numAlive();
 	
 	/**
-	 * Kill all entities in the group.
+	 * Kill all members of the group.
 	 */
-	public void killAll() {
-		for (CraftLivingEntity cle : group)
-			cle.setHealth(0);
-	}
+	public void killAll();
 	
 	/**
 	 * Teleport all members to given location
-	 * @param location to tp to
+	 * @param loc - location to tp to
 	 */
-	public void tpAll(Location loc) {
-		for (CraftLivingEntity cle : group)
-			cle.teleportTo(loc);
-	}
-	
-	/** 
-	 * Add new LivingEntity to group
-	 * @param new group member
-	 */
-	public void add(CraftLivingEntity cle) {
-		group.add(cle);
-	}
-	
-	/**
-	 * Remove LivingEntity from group
-	 * @param rejected group member
-	 */
-	public void remove(CraftLivingEntity cle) {
-		group.remove(cle);
-	}
+	public void tpAll(Location loc);
 	
 	/**
 	 * Removes all entities that do not belong in group
 	 */
-	public void removeUnwantedEntities() {}
+	public void removeUnwantedMembers();
 	
 	
 	
+	// TODO: Move to own class
 	/**
 	 * Returns a random location within the boundaries given
 	 * @param world - world where location exists
@@ -100,6 +44,7 @@ public class Group {
 	 * @param zMax - max z of volume
 	 * @return new location within given parameters
 	 */
+	/*
 	protected Location randomLocation(World world, double xMin, double xMax, double yMin, 
 			double yMax, double zMin, double zMax) {
 		double x, y, z;
@@ -116,5 +61,6 @@ public class Group {
 		
 		return new Location(world, x, y, z);
 	}
+	*/
 	
 }
